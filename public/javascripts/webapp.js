15c98c98249a04c8a8cd9427b22adae558ca9268
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-09-21 16:32:38 -0300

    Add Function#bind fallback

--- public/javascripts/webapp.js
+++ public/javascripts/webapp.js
@@ -1,8 +1,29 @@
 jQuery(function ($) {
 
   /*
-   * TODO: Check for Function.prototype.bind
-  */
+   * Check for Function.prototype.bind and define if not defined.
+   */
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+      if (typeof this !== "function") {
+        // closest thing possible to the ECMAScript 5 internal IsCallable function
+        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+      }
+
+      var aArgs = Array.prototype.slice.call(arguments, 1)
+        , fToBind = this
+        , fNOP = function () {}
+        , fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+            aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
+
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+
+      return fBound;
+    };
+  }
 
   var MIN_YEAR = 1960
     , MAX_YEAR = 2010
