b9e7c8b84880917d1a62411bfd36b524f2b93497
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-11-12 12:02:24 -0200

    Redirect to data host

--- app.rb
+++ app.rb
@@ -6,11 +6,15 @@ class App < Sinatra::Base
 
   helpers Helpers::Assets
 
+  APP_DOMAIN = 'data.180.com.uy'
+
   configure do
     set :views, root_path('views')
     set :public_folder, root_path('public')
     set :static_cache_control, [:public, max_age: 60 * 60 * 24]
-    set :environment, ENV['RACK_ENV'] || 'development'
+    set :environment, (ENV['RACK_ENV'] || 'development').to_sym
+
+    set :app_domain, settings.development? ? /localhost/ : APP_DOMAIN
 
     enable :static
   end
@@ -24,13 +28,17 @@ class App < Sinatra::Base
   end
 
   get %r{/(?:nombre/([^/]+)(?:/(\d+))?)?$} do |main_name, year|
-    cache_control :public, :must_revalidate, max_age: 60 * 60 * 24
-    names = (params[:others] || '').split(',')
-    names.unshift(main_name) if main_name
-    erb(:'index.html', layout: :'layout.html', locals: {
-      names: names.map(&:strip),
-      main_name: main_name,
-      year: year ? year.to_i : nil
-    })
+    if settings.app_domain === request.env['HTTP_HOST']
+      cache_control :public, :must_revalidate, max_age: 60 * 60 * 24
+      names = (params[:others] || '').split(',')
+      names.unshift(main_name) if main_name
+      erb(:'index.html', layout: :'layout.html', locals: {
+        names: names.map(&:strip),
+        main_name: main_name,
+        year: year ? year.to_i : nil
+      })
+    else
+      redirect("http://#{APP_DOMAIN}", 301)
+    end
   end
 end
